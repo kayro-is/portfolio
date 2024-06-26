58d91c3f40aba94c1e9120817c189dc3c728fcd3
kayro-is <luffykosuki@gmail.com>
2024-06-26 19:17:07 +0200

    modifié le chemin App.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './app.scss';
+
 import Contact from './component/Contact/Contact';
 import Cursor from './component/cursor/Cursor';
 import Hero from './component/hero/Hero';
@@ -6,6 +6,7 @@ import NavBar from './component/navBar/NavBar';
 import Parallax from './component/parallax/Parallax';
 import Portfolio from './component/portfolio/Portfolio';
 import Services from './component/Services/Services';
+import './App.scss';
 
 const App = () => {
   return ( 
